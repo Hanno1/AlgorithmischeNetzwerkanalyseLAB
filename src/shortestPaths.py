eb8fbdc06e13ad3e0820f1b8a2f91f4e4e412a69
maria gogolev <gogolevmaria@gmail.com>
2023-04-23 18:32:11 +0200

    add parallel version of all pairs shortest path

--- src/shortestPaths.py
+++ src/shortestPaths.py
@@ -1,19 +1,18 @@
 from collections import deque
 from Graph import Graph
+import multiprocessing
 
 def single_source_shortest_path(G : Graph, s):
     dist = {}
     for n in G.nodes:
-        dist[G.nodes[n].id] = float('inf')
+        dist[n] = float('inf')
     dist[s]=0
     queue = deque([s])
     visited = {s}
-    c = 0
     while queue:
         u = queue.popleft()
         for v in G.getNeighbors(u):
             if v not in visited:
-                c+=1
                 visited.add(v)
                 queue.append(v)
                 dist[v] = dist[u]+1
@@ -24,4 +23,31 @@ def all_pair_shortest_path(G : Graph):
     for v in G.nodes:
         v_id = G.nodes[v].id
         dist[v_id] = single_source_shortest_path(G,v_id)
+    return dist
+
+def _calc_shortest_paths(node_chunk, G):
+    dist_chunk = {}
+    for v in node_chunk:
+        v_id = G.nodes[v].id
+        dist_chunk[v_id] = single_source_shortest_path(G, v_id)
+    return dist_chunk
+
+def all_pair_shortest_path_parallel(G):
+    dist = {}
+    num_processes = multiprocessing.cpu_count()
+    nodes = list(G.nodes)
+    node_chunks = [nodes[i::num_processes] for i in range(num_processes)]
+
+    # create a process pool and map the node chunks to worker processes
+    with multiprocessing.Pool(num_processes) as pool:
+        results = []
+        for node_chunk in node_chunks:
+            result = pool.apply_async(_calc_shortest_paths, args=(node_chunk, G))
+            results.append(result)
+
+        # get the results from all processes and combine them into a single dictionary
+        for result in results:
+            dist_chunk = result.get()
+            dist.update(dist_chunk)
+
     return dist
