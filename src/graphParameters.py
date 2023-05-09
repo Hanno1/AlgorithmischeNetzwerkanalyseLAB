ad314ac2f8168424914c0218ce2a130e8a9a5cc1
maria gogolev <gogolevmaria@gmail.com>
2023-05-09 23:37:47 +0200

    compute clustering coefficients (not fully tested), and use defaultdict for degeneracy

--- src/graphParameters.py
+++ src/graphParameters.py
@@ -1,5 +1,6 @@
 from src.Graph import Graph
-from collections import deque
+from collections import deque, defaultdict
+from itertools import combinations
 
 def density(G: Graph):
     return (2*G.m)/(G.n*(G.n-1))
@@ -26,45 +27,57 @@ def h_index(G: Graph):
     return h_id
 
 def degeneracy(G: Graph):
-    deg_dict = {} # key: degree d, value: list of nodes that have degree d
+    degree_dict = defaultdict(set) # key: degree d, value: set of nodes that have degree d
     node_to_deg = {} # maps node to its degree
-    for i in G.internal_ids_node_ids:
-        deg = len(G.edges[i])
-        node_to_deg[i] = deg
-        if deg in deg_dict:
-            deg_dict[deg].add(i)
-        else:
-            deg_dict[deg] = set([i])
-    removed = set() # or "to-be"-removed
+    for node in G.internal_ids_node_ids:
+        degree = len(G.edges[node])
+        node_to_deg[node] = degree
+        degree_dict[degree].add(node)
+
+    nodes_removed = set() # list of removed nodes
     degeneracy_order = []
-    while deg_dict:
-        x = min(deg_dict.keys())
-        nodes_deg_x = deque(deg_dict[x])
+    while degree_dict:
+        x = min(degree_dict.keys())
+        nodes_deg_x = deque(degree_dict[x])
         while nodes_deg_x:
             node = nodes_deg_x.pop()
-            removed.add(node) # sometimes nodes are added twice, doesn't matter
+            nodes_removed.add(node)
             degeneracy_order.append(G.internal_ids_node_ids[node])
-            neighbors = set(G.get_internal_neighbors(node))-removed
+            neighbors = set(G.get_internal_neighbors(node)) - set(nodes_removed)
             # iterate over all neighbors of "removed" node, fix their degrees
             for n in neighbors:
-                if n in removed: # we want to know if n is already staged for removal
+                if n in nodes_removed: # we want to know if n is already staged for removal
                 # <-> "n in nodes_deg_x", but testing this is inefficient
                     continue
                 neighb_deg = node_to_deg[n]
-                deg_dict[neighb_deg].remove(n)
-                if len(deg_dict[neighb_deg]) == 0:
-                    del deg_dict[neighb_deg]
+                degree_dict[neighb_deg].remove(n)
+                if not degree_dict[neighb_deg]:
+                    del degree_dict[neighb_deg]
                 # if the degree is smaller than degeneracy, add neighbor to queue
                 if neighb_deg <= x:
                     nodes_deg_x.append(n)
-                    removed.add(n)
+                    nodes_removed.add(n)
                 else: # otherwise register neighbor as member of decremented degree
-                    node_to_deg[n] = neighb_deg-1
-                    if neighb_deg-1 in deg_dict:
-                        deg_dict[neighb_deg-1].add(n)
-                    else:
-                        deg_dict[neighb_deg-1] = set([n])
-        del deg_dict[x]
+                    node_to_deg[n] -= 1
+                    degree_dict[node_to_deg[n]].add(n)
+        del degree_dict[x]
     return x, degeneracy_order
 
-    
+def global_clustering_coefficient(G: Graph):
+    triangles = 0
+    triples = 0
+    for node in G.internal_ids_node_ids:
+        neighbors = G.get_internal_neighbors(node)
+        k = len(neighbors)
+        if k < 2:
+            continue
+        triples += k * (k-1) / 2
+        triangles += sum(1 for u, v in combinations(neighbors, 2) if v in G.edges[u])
+    return (triangles / triples)
+
+def local_clustering_coefficient(G: Graph, node):
+    internal_node = G.node_ids_internal_ids[node]
+    neighbors = G.get_internal_neighbors(internal_node)
+    k = len(neighbors)
+    edges_neighborhood = sum(1 for u, v in combinations(neighbors, 2) if v in G.edges[u])
+    return (edges_neighborhood / ((k * (k-1))/2))
--- test_/UnitTest_graphParams.py
+++ test_/UnitTest_graphParams.py
@@ -3,6 +3,7 @@ from src.Graph import Graph
 import HelperClass as HelperClass
 from unittest import TestCase
 import unittest
+import networkx as nx
 
 
 class TestShortestPaths(TestCase):
@@ -60,6 +61,47 @@ class TestShortestPaths(TestCase):
                 if degeneracy_order.index(nn) > n_index:
                     neighb_higher_order += 1
             self.assertGreaterEqual(degeneracy,neighb_higher_order)
+
+    def test_cluster(self):
+        G = Graph()
+        nxG = nx.Graph()
+        nxG.add_node(1)
+        nxG.add_edge(3,2)
+        nxG.add_edge(3,5)
+        nxG.add_edge(3,6)
+        nxG.add_edge(4,2)
+        nxG.add_edge(4,5)
+        nxG.add_edge(4,7)
+        nxG.add_edge(4,8)
+        nxG.add_edge(4,9)
+        nxG.add_edge(9,7)
+        nxG.add_edge(9,8)
+        nxG.add_edge(9,10)
+        nxG.add_edge(10,7)
+        nxG.add_edge(8,7)
+        nxG.add_edge(10,8)
+
+        G.add_node(1)
+        G.add_edge(3,2)
+        G.add_edge(3,5)
+        G.add_edge(3,6)
+        G.add_edge(4,2)
+        G.add_edge(4,5)
+        G.add_edge(4,7)
+        G.add_edge(4,8)
+        G.add_edge(4,9)
+        G.add_edge(9,7)
+        G.add_edge(9,8)
+        G.add_edge(9,10)
+        G.add_edge(10,7)
+        G.add_edge(8,7)
+        G.add_edge(10,8)
+        glob_cluster = gP.global_clustering_coefficient(G)
+        loc_cluster = gP.local_clustering_coefficient(G,"4")
+        loc_cluster_nx = nx.clustering(nxG)
+        print(glob_cluster)
+        print(loc_cluster)
+        print(loc_cluster_nx)
 
 if __name__ == '__main__':
     unittest.main()
