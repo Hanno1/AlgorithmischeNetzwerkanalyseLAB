b520d7ee33b99c7d1c05fde1a4286e920e80140c
Hanno1 <hanno.barschel@web.de>
2023-06-01 21:16:46 +0200

    task2/hanno started implementing my own centrality, writing tests now and then starting with the presentation

--- src/networkCentrality/myCentrality.py
+++ src/networkCentrality/myCentrality.py
@@ -1,54 +1,111 @@
 import numpy as np
 from src.Graph import Graph
 import src.shortestPaths as Sp
+import random
 
 
 class OwnCentrality:
-    def __init__(self, G: Graph):
+    def __init__(self, G: Graph, p=2, init=None, k_uniform_nodes=None):
+        """
+        :param G: Graph
+        :param p: power value, the distances will be scaled with this
+        :param init: initial centrality as a dictionary, if this is None, all centrality will be set to 0
+        :param k_uniform_nodes: if we want to make the algorithm fast, we will use only k_uniform_nodes, instead of all
+
+        Class to compute centrality of a given graph. The centrality for a node is computed by summing the product
+        of the centrality and the distance to the power of p of all connected nodes
+
+        There is a fast Mode, since it might be impractical to compute n BFS's there we approximate the centrality
+        by using the distances to k_uniform_nodes which are uniformly chosen from the Graph G.
+        """
         self.G = G
+        self.p = p
+        if k_uniform_nodes is None:
+            self.fast = False
+            self.distances = Sp.all_pairs_shortest_path_single(self.G)
+        else:
+            self.fast = True
+            # we cant use internal id since there might be gaps in between
+            all_nodes = list(G.node_ids_internal_ids.keys())
+            # choose uniform k_centrality nodes
+            self.distances = dict()
+            for _ in range(k_uniform_nodes):
+                node = random.choice(all_nodes)
+                self.distances[node] = Sp.single_source_shortest_path(self.G, node)
+
+        if init is not None:
+            self.initial_centrality = init
+        else:
+            self.initial_centrality = dict()
+            for node in self.G.node_ids_internal_ids:
+                self.initial_centrality[node] = 1
+
+    def _single_node_centrality_fast(self, v):
+        """
+        :param v: node for which to compute the single node centrality
+        :return: node centrality of v
+
+        compute the node centrality of v in fast mode - meaning we will only look at the distances from
+        the k nodes we uniformly drew
+        """
+        node_centrality = 0
+        for node in self.distances:
+            if node != v and v in self.distances[node]:
+                node_centrality += self.initial_centrality[node] * (1 / self.distances[node][v] ** self.p)
+        return node_centrality
+
+    def _single_node_centrality_slow(self, v):
+        """
+        :param v: node for which to compute the single node centrality
+        :return: node centrality of v
+
+        compute the node centrality of v in slow mode - here v is in self.distances and can be accessed
+        """
+        node_centrality = 0
+        actual_distances = self.distances[v]
+        for node in actual_distances:
+            if node != v:
+                node_centrality += self.initial_centrality[node] * (1 / actual_distances[node] ** self.p)
+        return node_centrality
 
     def single_node_centrality(self, v):
         """
         compute centrality of node with external id v
 
-        :param v:
-        :return:
+        :param v: node v for which to compute shortest paths
+        :return: centrality of node v
         """
         v = str(v)
-        distances = Sp.single_source_shortest_path(self.G, v)
-        node_degrees = dict()
-        for node in self.G.node_ids_internal_ids:
-            node_degrees[node] = self.G.get_node_degree(node)
+        if self.fast:
+            if v in self.distances:
+                return self._single_node_centrality_slow(v)
+            return self._single_node_centrality_fast(v)
+        return self._single_node_centrality_slow(v)
 
-        node_centrality = 0
-        for w in distances:
-            if w != v:
-                node_centrality += node_degrees[w] * (1/distances[w]**2)
-        return node_centrality
+    def all_nodes_centrality(self):
+        """
+        :return: dictionary containing all nodes centrality
 
-    def _init_nodes_centrality(self):
-        node_degrees = dict()
-        for node in self.G.node_ids_internal_ids:
-            node_degrees[node] = self.G.get_node_degree(node)
-        return node_degrees
+        computes the node centrality of all nodes in G using the private functions for single node centrality
+        """
+        new_centrality = dict()
+        if self.fast:
+            for node in self.initial_centrality:
+                if node in self.distances:
+                    new_centrality[node] = self._single_node_centrality_slow(node)
+                else:
+                    new_centrality[node] = self._single_node_centrality_fast(node)
+            return new_centrality
+        for node in self.initial_centrality:
+            new_centrality[node] = self._single_node_centrality_slow(node)
+        return new_centrality
 
-    def all_nodes_centrality(self, i=1):
-        distances = Sp.all_pairs_shortest_path_single(self.G)
-        initial_centralities = self._init_nodes_centrality()
+    def most_central_node(self):
+        """
+        :return: the most central node and the corresponding centrality - it will only return 1 node though!
+        """
+        result = self.all_nodes_centrality()
 
-        for _ in range(i):
-            new_centralities = dict()
-            for node in initial_centralities:
-                new_centralities[node] = 0
-                distances_current = distances[node]
-                for v in distances_current:
-                    if v != node:
-                        new_centralities[node] += initial_centralities[v] * (1 / distances_current[v]**2)
-            initial_centralities = new_centralities
-        return initial_centralities
-
-    def most_central_node(self, i=1):
-        result = self.all_nodes_centrality(i)
         max_centrality = 0
         most_central_nodes = []
         eps = np.finfo(float).eps
@@ -58,16 +115,21 @@ class OwnCentrality:
                 most_central_nodes = [node]
             elif max_centrality - eps < result[node] < max_centrality + eps:
                 most_central_nodes.append(node)
-        return most_central_nodes
+        return most_central_nodes, max_centrality
 
-    def k_central_nodes(self, k, i=1):
+    def k_central_nodes(self, k):
+        """
+        :param k: number of nodes to return
+        :return: number and centralities of the k most central nodes
+        """
         k_central_nodes = []
         centralities = []
-        result = self.all_nodes_centrality(i)
+        result = self.all_nodes_centrality()
 
         for node in result:
             centrality = result[node]
             if len(k_central_nodes) < k:
+                # insert new node, but at the right place, so that the list is still sorted
                 inserted = False
                 for index in range(len(k_central_nodes)):
                     if centrality < centralities[index]:
