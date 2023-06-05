fd7136330989739c663edb41181108c60d5b7a42
maria gogolev <gogolevmaria@gmail.com>
2023-06-05 17:15:52 +0200

    finished closeness, approximate closeness and degree centralities, wrote short summary in presentation

--- src/networkCentrality/basicCentralities.py
+++ src/networkCentrality/basicCentralities.py
@@ -1,16 +1,18 @@
 from src.Graph import Graph
-from src.shortestPaths import all_pairs_shortest_path, single_source_shortest_path
+from src.shortestPaths import all_pairs_shortest_path, single_source_shortest_path, connected_components
 import random
 import numpy as np
 
 def closeness_centrality_approx(G: Graph, k_approx = None, k = None):
     """
     Approximates the closeness centrality for nodes in a graph using random sampling.
+    In case the graph has multiple components, the harmonic centrality is calculated.
 
     Parameters:
         G (Graph): The graph object for which closeness centrality is to be approximated.
         k_approx (int, optional): The number of nodes to sample for approximation. if not provided, the function
             uses the square root of the number of nodes in the graph.
+            in case k_approx is larger then the number of nodes in the Graph, the regular closeness_centrality is used.
         k (int, optional): The number of top nodes to return. Default is None, which returns centrality values for all nodes.
 
     Returns:
@@ -26,16 +28,35 @@ def closeness_centrality_approx(G: Graph, k_approx = None, k = None):
     """
     node_ids = list(G.node_ids_internal_ids.keys())
     if not k_approx:
-        k_approx = max(int(np.sqrt(G.n)), 10)
-    k_choices =  random.choices(node_ids, k=k_approx)
-    node_centralities={}
-    avg = np.zeros(G.n)
-    for n in k_choices:
-        shortest_paths = single_source_shortest_path(G,n)
-        node_centralities[n]=(G.n-1)/sum(shortest_paths.values())
-        avg += np.array([shortest_paths[node] for node in node_ids])
+        k_approx = max(int(np.sqrt(G.n)), 10) # choosing a "moderate amount" of nodes. just an estimation
 
-    avg = k_approx/avg
+    if k_approx>=G.n:
+        return closeness_centrality(G, k = k)
+
+    k_choices =  random.choices(node_ids, k=k_approx)
+
+    all_shortest_paths = {}
+    for n in k_choices:
+        all_shortest_paths[n] = single_source_shortest_path(G,n)
+
+    node_centralities={}
+    cc = connected_components(G)
+    avg = np.zeros(G.n)
+    if len(cc)>1:
+        for n in k_choices:
+            shortest_paths = all_shortest_paths[n]
+            node_centralities[n]=sum([1/d if d >0 else 0 for d in shortest_paths.values()])/(G.n-1)
+            for node_d in node_ids:
+                if node_d in shortest_paths and node_d != n:
+                    avg[node_ids.index(node_d)] += 1/shortest_paths[node_d]
+        avg = avg/k_approx
+
+    else:
+        for n in k_choices:
+            shortest_paths = single_source_shortest_path(G,n)
+            node_centralities[n]=(G.n-1)/sum(shortest_paths.values())
+            avg += np.array([shortest_paths[node] for node in node_ids])
+        avg = k_approx/avg
 
     for n in set(G.node_ids_internal_ids)-node_centralities.keys(): # not-chosen nodes
         idx = node_ids.index(n)
@@ -52,6 +73,7 @@ def closeness_centrality_approx(G: Graph, k_approx = None, k = None):
 def closeness_centrality(G: Graph, node = None, k = None):
     """
     Calculate closeness centrality for nodes in a graph.
+    In case the graph has multiple components, the harmonic centrality is calculated.
 
     Parameters:
     - G (Graph): The graph object.
@@ -67,18 +89,25 @@ def closeness_centrality(G: Graph, node = None, k = None):
         raise Exception("invalid combination of arguments: You can either get top-k central nodes or centrality for one specific node")
     if node:
         shortest_paths = single_source_shortest_path(G,node)
-        return (G.n-1)/sum(shortest_paths.values())
+        if len(shortest_paths)<G.n:
+            return sum([1/d if d >0 else 0 for d in shortest_paths.values()])/(G.n-1)
+        else:
+            return (G.n-1)/sum(shortest_paths.values())
     all_shortest_paths = all_pairs_shortest_path(G)
     node_centralities = {}
-    for n in all_shortest_paths:
-        shortest_paths= all_shortest_paths[n]
-        node_centralities[n]=(G.n-1)/sum(shortest_paths.values())
+    if len(next(iter(all_shortest_paths.values())))< G.n: #harmonic centrality for multi-component graphs
+        for n in all_shortest_paths:
+            shortest_paths= all_shortest_paths[n]
+            node_centralities[n]=sum([1/d if d >0 else 0 for d in shortest_paths.values()])/(G.n-1)
+    else:
+        for n in all_shortest_paths:
+            shortest_paths= all_shortest_paths[n]
+            node_centralities[n]=(G.n-1)/sum(shortest_paths.values())
     if k:
         top_k_nodes = sorted(node_centralities, key=node_centralities.get, reverse=True)[:k]
         top_k_centralities = {node: node_centralities[node] for node in top_k_nodes}
         return top_k_centralities
     return node_centralities
-
 
 def degree_centrality(G: Graph, node = None, k = None):
     """
@@ -103,11 +132,9 @@ def degree_centrality(G: Graph, node = None, k = None):
     for n in G.node_ids_internal_ids:
         n_internal = G.node_ids_internal_ids[n]
         c = len(G.get_internal_neighbors(n_internal))
-        node_centralities[n] = c
+        node_centralities[n] = c/(G.n-1)
     if k:
         top_k_nodes = sorted(node_centralities, key=node_centralities.get, reverse=True)[:k]
         top_k_centralities = {node: node_centralities[node] for node in top_k_nodes}
         return top_k_centralities
     return node_centralities
-
-    
