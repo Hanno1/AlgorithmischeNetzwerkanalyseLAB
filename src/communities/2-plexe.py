2846cf33bec26f2da9fea4b51914d5ce6d4eaf8b
Hanno1 <hanno.barschel@web.de>
2023-06-09 15:02:44 +0200

    feature/2-Plexe working 2-plex Algorithm with 2 improvements (a and c from the tasksheet)

--- src/communities/2-plexe.py
+++ src/communities/2-plexe.py
@@ -1,69 +1,68 @@
+import copy
+
 from src.Graph import Graph
-from src.printGraph import draw_graph
 import test_.HelperClass as Hc
 
 
-def test_2_plex(G: Graph):
-    n = G.n
-    for internal_node in G.internal_ids_node_ids:
-        if len(G.get_internal_neighbors(internal_node)) < n-2:
+def test_2_plex(g: Graph, nodes):
+    n = len(nodes)
+    for node in nodes:
+        if len(g.get_neighbors(node) & nodes) < n-2:
             return False
     return True
 
 
 def filter_2_plex(list_):
     maximal_length = 0
-    new_list = []
-    for G in list_:
-        if G.n > maximal_length:
-            maximal_length = G.n
-    for G in list_:
-        if G.n == maximal_length:
-            return G
+    for g in list_:
+        if len(g) > maximal_length:
+            maximal_length = len(g)
+    for g in list_:
+        if len(g) == maximal_length:
+            return g
 
 
-def search_2_plex_rec(H: Graph):
-    H.print_nodes()
-    if test_2_plex(H):
-        print("found 2 plex")
-        if H.n > 2:
-            all_2_plex.append(H)
-        return
+def search_2_plex_rec(orig_graph: Graph, actual_nodes, permanent, max_2_plex):
+    current_n = len(actual_nodes)
+    if current_n <= len(max_2_plex):
+        return None
+    if test_2_plex(orig_graph, actual_nodes):
+        if current_n > len(max_2_plex):
+            return copy.deepcopy(actual_nodes)
+        return None
+    for v in actual_nodes:
+        neighbors_v = orig_graph.get_neighbors(v) & actual_nodes
+        if v not in permanent and len(neighbors_v) < current_n - 2:
+            actual_nodes.remove(v)
+            res = search_2_plex_rec(orig_graph, actual_nodes, permanent, max_2_plex)
+            if res:
+                max_2_plex = res
+            actual_nodes.add(v)
 
-    node_id_internal_id = H.node_ids_internal_ids
-    for v in node_id_internal_id:
-        print(v)
-        # Case 1: v is not in max 2 plex
-        # remove v
-        newGraph = H.copy_graph()
-        newGraph.remove_node(v)
+            # case 2 - v is in 2-plex and therefore permanent
+            permanent.append(v)
 
-        search_2_plex_rec(newGraph)
+            not_neighbors = actual_nodes - (neighbors_v | {v})
+            actual_nodes = neighbors_v | {v}
+            # none of the not neighbors
+            res = search_2_plex_rec(orig_graph, actual_nodes, permanent, max_2_plex)
+            if res:
+                max_2_plex = res
 
-        # Case 2: v is in max 2 plex -> all neighbors might be too as well as one additional neighbor
-        # remove not neighborhood
-        neighborhood_v = H.get_neighbors(v)
+            # at most one of the not neighbors
+            for u in not_neighbors:
+                actual_nodes.add(u)
+                res = search_2_plex_rec(orig_graph, actual_nodes, permanent, max_2_plex)
+                if res:
+                    max_2_plex = res
+                actual_nodes.remove(u)
 
-        for u in neighborhood_v:
-            newGraph = H.copy_graph()
-            newGraph.remove_node(u)
-            search_2_plex_rec(newGraph)
+            # in the top the path can be still possible
+            permanent.remove(v)
+            break
+    return max_2_plex
 
 
-# G = Graph("../../networks/out.ucidata-zachary_")
-# draw_graph(G, label_on=True)
-G = Graph()
-G.add_edge(10, 11)
-G.add_edge(10, 13)
-G.add_edge(11, 12)
-G.add_edge(12, 13)
-G.add_edge(13, 14)
-G.add_edge(14, 15)
-G.add_node(0)
-# G, _, _ = Hc.create_graph()
+G = Graph("../../networks/out.ucidata-zachary_")
 
-all_2_plex = []
-search_2_plex_rec(G)
-
-filtering = filter_2_plex(all_2_plex)
-filtering.print_edges()
+print(search_2_plex_rec(G, G.get_nodes(), [], []))
